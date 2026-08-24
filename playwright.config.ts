fb407908dd6f96c662118aeb75efc696f93c6297
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-08-24 19:39:24 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,17 +26,21 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-   baseURL: 'https://katalon-demo-cura.herokuapp.com/',
+    baseURL: 'https://katalon-demo-cura.herokuapp.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+      }
     },
     /*
     {
