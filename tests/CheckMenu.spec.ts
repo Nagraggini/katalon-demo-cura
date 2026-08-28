0517d21d03973f820af0fe1c8a6d02d73bb3c35a
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-08-28 15:11:23 +0200

    feat: create CheckMenu test

--- tests/CheckMenu.spec.ts
+++ tests/CheckMenu.spec.ts
@@ -1,20 +1,24 @@
-import { test, expect } from '../fixtures/BaseTest';
+import { test, expect } from "../fixtures/BaseTest";
 
-test('Main menu buttons check', async ({ page, landingPage, loginPage }) => {
-  await landingPage.navigate();
+test("Main menu buttons check", async ({ page, landingPage, loginPage }) => {
+    await landingPage.navigate();
 
-  await landingPage.openMenu();
-  await expect(landingPage.home).toBeVisible();
-  await landingPage.home.click();
+    await test.step("Check home in main menu", async () => {
+        await landingPage.openMenu();
+        await expect(landingPage.home).toBeVisible();
+        await landingPage.home.click();
+        await expect(landingPage.page).toHaveURL(
+            /katalon-demo-cura\.herokuapp\.com\/$/,
+        );
+        await expect(landingPage.heading).toBeVisible();
+    });
 
-  await expect(page).toHaveURL(/katalon-demo-cura\.herokuapp\.com\/$/);
-  await expect(landingPage.heading).toBeVisible();
-
-  await landingPage.openMenu();
-  await expect(landingPage.login).toBeVisible();
-  await landingPage.login.click();
-
-  await expect(page).toHaveURL(/profile\.php#login$/);
-  await expect(loginPage.loginHeading).toBeVisible();
-  await expect(loginPage.loginMessage).toBeVisible();
+    await test.step("Check login in main menu", async () => {
+        await landingPage.openMenu();
+        await expect(landingPage.login).toBeVisible();
+        await landingPage.login.click();
+        await expect(landingPage.page).toHaveURL(/profile\.php#login$/);
+        await expect(loginPage.loginHeading).toBeVisible();
+        await expect(loginPage.loginMessage).toBeVisible();
+    });
 });
