15e3adfd43cb0eff2cb71b92f9695dc7d13c6049
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-09-19 10:27:16 +0200

    modify tags and annotations in FullMakeAppointment test

--- tests/FullMakeAppointment.spec.ts
+++ tests/FullMakeAppointment.spec.ts
@@ -1,93 +1,107 @@
 import { test, expect } from "../fixtures/BaseTest";
 import { Appointment } from "../models/Appointment";
 
-test("Full appointment workflow with login", async ({
-    page,
-    loginPage,
-    makeAppointmentPage,
-    summaryPage,
-    sideMenuPage,
-    historyPage,
-}, testInfo) => {
-    // Mark a test with tags or dynamic description.
-    testInfo.annotations.push({
-        type: "category",
-        description: "smoke-test",
-    });
-
-    await test.step("Login", async () => {
-        await loginPage.navigate();
-        await loginPage.login();
-    });
-
-    const appointmentDate = new Date();
-    // A date one week later than today.
-    appointmentDate.setDate(appointmentDate.getDate() + 7);
-
-    const visitDate = appointmentDate.toLocaleDateString("en-GB");
-
-    await test.step("Make an Appointment", async () => {
-        await expect(makeAppointmentPage.page).toHaveURL(/#appointment$/);
-        await expect(makeAppointmentPage.heading).toBeVisible();
-
-        await makeAppointmentPage.createAppointment({
-            facility: "Seoul CURA Healthcare Center",
-            hospitalReadmission: false,
-            program: "Medicare",
-            visitDate: visitDate,
-            comment: "Christmas annual check-up.",
+test(
+    "Full appointment workflow with login",
+    {
+        tag: ["@smoke", "@appointment"],
+    },
+    async (
+        {
+            page,
+            loginPage,
+            makeAppointmentPage,
+            summaryPage,
+            sideMenuPage,
+            historyPage,
+        },
+        testInfo,
+    ) => {
+        // Mark a test with tags or dynamic description.
+        testInfo.annotations.push({
+            type: "test-case",
+            description: "TC-APPOINTMENT-01",
         });
-    });
 
-    await test.step("Check Appointment on SummaryPage", async () => {
-        await expect(summaryPage.page).toHaveURL(/#summary$/);
-        await expect(summaryPage.heading).toBeVisible();
-        await expect(summaryPage.facilityValue).toHaveText(
-            "Seoul CURA Healthcare Center",
-        );
-        await expect(summaryPage.readmissionValue).toHaveText("No");
-        await expect(summaryPage.healthcareProgramValue).toHaveText("Medicare");
-        await expect(summaryPage.visitDateValue).toHaveText(visitDate);
-        await expect(summaryPage.commentValue).toHaveText(
-            "Christmas annual check-up.",
-        );
+        await test.step("Login", async () => {
+            await loginPage.navigate();
+            await loginPage.login();
+        });
 
-        await expect(summaryPage.goToHomePageButton).toHaveAttribute(
-            "href",
-            "https://katalon-demo-cura.herokuapp.com/",
-        );
-    });
+        const appointmentDate = new Date();
+        // A date one week later than today.
+        appointmentDate.setDate(appointmentDate.getDate() + 7);
 
-    await test.step("Check Appointment in History", async () => {
-        await sideMenuPage.openMenu();
-        await sideMenuPage.history.click();
-        await expect(sideMenuPage.page).toHaveURL(/history\.php#history$/);
-        await expect(historyPage.h2).toBeVisible();
+        const visitDate = appointmentDate.toLocaleDateString("en-GB");
 
-        const historyItems: Appointment[] = await historyPage.getAppointments();
+        await test.step("Make an Appointment", async () => {
+            await expect(makeAppointmentPage.page).toHaveURL(/#appointment$/);
+            await expect(makeAppointmentPage.heading).toBeVisible();
 
-        // Check the history isn't empty.
-        await expect(historyItems.length).toBeGreaterThan(0);
+            await makeAppointmentPage.createAppointment({
+                facility: "Seoul CURA Healthcare Center",
+                hospitalReadmission: false,
+                program: "Medicare",
+                visitDate: visitDate,
+                comment: "Christmas annual check-up.",
+            });
+        });
 
-        await expect(historyItems[0].visitDate).toBe(visitDate);
-        await expect(historyItems[0].facility).toBe(
-            "Seoul CURA Healthcare Center",
-        );
-        await expect(historyItems[0].hospitalReadmission).toBe(false);
-        await expect(await historyItems[0].healthcareProgram).toBe("Medicare");
-        await expect(historyItems[0].comment).toBe(
-            "Christmas annual check-up.",
-        );
+        await test.step("Check Appointment on SummaryPage", async () => {
+            await expect(summaryPage.page).toHaveURL(/#summary$/);
+            await expect(summaryPage.heading).toBeVisible();
+            await expect(summaryPage.facilityValue).toHaveText(
+                "Seoul CURA Healthcare Center",
+            );
+            await expect(summaryPage.readmissionValue).toHaveText("No");
+            await expect(summaryPage.healthcareProgramValue).toHaveText(
+                "Medicare",
+            );
+            await expect(summaryPage.visitDateValue).toHaveText(visitDate);
+            await expect(summaryPage.commentValue).toHaveText(
+                "Christmas annual check-up.",
+            );
 
-        await expect(summaryPage.goToHomePageButton).toHaveAttribute(
-            "href",
-            "https://katalon-demo-cura.herokuapp.com/",
-        );
-    });
+            await expect(summaryPage.goToHomePageButton).toHaveAttribute(
+                "href",
+                "https://katalon-demo-cura.herokuapp.com/",
+            );
+        });
 
-    await test.step("Logout and check it.", async () => {
-        await sideMenuPage.openMenu();
-        await sideMenuPage.logout.click();
-        await expect(sideMenuPage.login).toBeVisible();
-    });
-});
+        await test.step("Check Appointment in History", async () => {
+            await sideMenuPage.openMenu();
+            await sideMenuPage.history.click();
+            await expect(sideMenuPage.page).toHaveURL(/history\.php#history$/);
+            await expect(historyPage.h2).toBeVisible();
+
+            const historyItems: Appointment[] =
+                await historyPage.getAppointments();
+
+            // Check the history isn't empty.
+            await expect(historyItems.length).toBeGreaterThan(0);
+
+            await expect(historyItems[0].visitDate).toBe(visitDate);
+            await expect(historyItems[0].facility).toBe(
+                "Seoul CURA Healthcare Center",
+            );
+            await expect(historyItems[0].hospitalReadmission).toBe(false);
+            await expect(await historyItems[0].healthcareProgram).toBe(
+                "Medicare",
+            );
+            await expect(historyItems[0].comment).toBe(
+                "Christmas annual check-up.",
+            );
+
+            await expect(summaryPage.goToHomePageButton).toHaveAttribute(
+                "href",
+                "https://katalon-demo-cura.herokuapp.com/",
+            );
+        });
+
+        await test.step("Logout and check it.", async () => {
+            await sideMenuPage.openMenu();
+            await sideMenuPage.logout.click();
+            await expect(sideMenuPage.login).toBeVisible();
+        });
+    },
+);
